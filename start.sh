1c6906f7205038c986e90c5a42f5324b8d599317
ittp <61617660+ittp@users.noreply.github.com>
2021-08-13 22:28:58 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 echo "start"
+ssh pavel@hub.ittps.ru -p 2222 -T -L 0.0.0.0:3322:0.0.0.0:22 -vvv -N -i ./hub.key
 
-ssh root@tun.ittps.ru -v
+ssh root@avt.ittps.ru -p 2222 -T -L 0.0.0.0:3322:0.0.0.0:22 -vvv -N
