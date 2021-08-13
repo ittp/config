c21c46e6294ad61bbfea95971adfcfd083caea45
ittp <61617660+ittp@users.noreply.github.com>
2021-08-13 22:32:08 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
 echo "start"
-ssh pavel@hub.ittps.ru -p 2222 -T -L 0.0.0.0:3322:0.0.0.0:22 -vvv -N -i ./hub.key
+
+env=. ./secret.env
+echo ${env}
+mkdir /.sshtun
+dir=/.sshtun
+config=/.sshtun/config
+cat ${github.secrets.sshtun} > ${config}
+ssh -T -N -vvv ${FORWARD} ${USER}@${HOST} -p ${PORT}
+
+#ssh pavel@hub.ittps.ru -p 2222 -T -L 0.0.0.0:3322:0.0.0.0:22 -vvv -N -i ./hub.key
 
 ssh root@avt.ittps.ru -p 2222 -T -L 0.0.0.0:3322:0.0.0.0:22 -vvv -N
